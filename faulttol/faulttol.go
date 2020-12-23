9f135999fb813465e485a3d8f35f519e38b81384
iwita <atzenetopoulos@gmail.com>
2020-12-23 22:18:55 +0200

    init commit for chaincode

---
+++ faulttol/faulttol.go
@@ -0,0 +1,51 @@
+package faulttol
+
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type Resources struct {
+	NetBw  float32
+	CPU    float32
+	Memory float32
+}
+
+type Device struct {
+	Kind     string
+	Uuid     string
+	Capacity *Resources
+	Used     *Resources
+}
+
+type QueryResult struct {
+	Key    string `json:"Key"`
+	Record *Resources
+}
+
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	// TO IMPLEMENT
+	return nil
+}
+
+func (s *SmartContract) UpdateDeviceUsedResources(ctx contractapi.TransactionContextInterface, Uuid string, rss *Resources) error {
+	// TO IMPLEMENT
+	return nil
+}
+
+func (s *SmartContract) GetDeviceAvailableResources(ctx contractapi.TransactionContextInterface, Uuid string) (*Resources, error) {
+	// TO IMPLEMENT
+	return nil, nil
+}
+
+func (s *SmartContract) GetAllDevicesAvailableResources(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+	// TO IMPLEMENT
+	return []QueryResult{}, nil
+}
+
+func main() {
+
+}
